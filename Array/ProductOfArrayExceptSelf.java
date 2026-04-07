6c0e4eb67dd33d0f27c5837b4e5440b7ef1960e2
Soumo Sarkar <soumosarkar297@gmail.com>
2026-04-07 15:11:42 +0530

    Add implementation for LeetCode Problem 238: Product of Array Except Self

---
+++ Array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+
+/**
+ * LeetCode Problem: 238. Product of Array Except Self
+ */
+class Solution {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int prefix = 1, postfix = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return ans;
+    }
+}
+
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(Solution.productExceptSelf(nums)));
+    }
+
+}
